ca1900a1c9752b6e5d295b51f0cd5d56c218eba1
jtitor <1165137798@qq.com>
2024-11-21 23:54:41 +0800

    commit

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def download_model(model_name):
     if not os.path.isfile(local_path):
         os.makedirs('pretrained_models', exist_ok=True)
         web_path = f'https://www.dl.dropboxusercontent.com/scl/fi/cxedbs4da5ugjq5wg3zrg/last.pt?rlkey=8otgrdkno0nd89po3dpwngwcc&st=apcc645o&dl=0'
-        download_url(web_path, 'pretrained_models', filename=model_name)
+        download_url(web_path, '/root/pretrained_models', filename=model_name)
     model = torch.load(local_path, map_location=lambda storage, loc: storage)
     return model
 
