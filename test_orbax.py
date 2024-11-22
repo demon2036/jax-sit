de17f1178626ce0ec4418d18cb1ed1835f79b5fe
jtitor <1165137798@qq.com>
2024-11-22 22:13:16 +0800

    commit

--- test_orbax.py
+++ test_orbax.py
@@ -5,13 +5,14 @@ import numpy as np
 import tqdm
 from PIL import Image
 import orbax.checkpoint as ocp
+from flax.training.common_utils import shard_prng_key
 from orbax.checkpoint.utils import fully_replicated_host_local_array_to_global_array
 
 jax.distributed.initialize()
 
 rng=jax.random.PRNGKey(0)+jax.process_index()
 
-
+rng=shard_prng_key(rng)
 rng=fully_replicated_host_local_array_to_global_array(rng)
 
 print(rng)
