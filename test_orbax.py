42c79dc38506b315f24bb06fbab072ff6b48d752
jtitor <1165137798@qq.com>
2024-11-22 22:02:12 +0800

    commit

---
+++ test_orbax.py
@@ -0,0 +1,25 @@
+import os
+
+import jax
+import numpy as np
+import tqdm
+from PIL import Image
+import orbax.checkpoint as ocp
+
+jax.distributed.initialize()
+
+rng=jax.random.PRNGKey(0)+jax.process_index()
+
+print(rng)
+
+checkpointer = ocp.AsyncCheckpointer(ocp.PyTreeCheckpointHandler())
+
+
+
+checkpointer.save('gs://roger-center-2b/test_orbax',{
+    'rng':rng
+})
+
+restore_rng=checkpointer.restore('gs://roger-center-2b/test_orbax')
+print(f'{restore_rng=}   {rng=}')
+
