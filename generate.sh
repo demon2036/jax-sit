25409aed9bbbdee253525b152918c13545eca43c
jtitor <1165137798@qq.com>
2024-11-22 23:22:22 +0800

    commit

---
+++ generate.sh
@@ -0,0 +1,21 @@
+#echo "${SCRIPT_PATHS[@]}"
+#SCRIPT_PATHS=$1
+SCRIPT_PATHS=("$@")
+#echo "Array content: $1"
+
+#echo "Array content: ${SCRIPT_PATHS[@]}"
+for script in "${SCRIPT_PATHS[@]}"; do
+    echo " $script"
+    pkill -9 -f python
+    sudo rm /tmp/libtpu_lockfile
+    source ~/miniconda3/bin/activate base;
+#    python -u test.py
+
+#    python -u test_orbax.py
+    python -u generate.py
+
+
+#    python -u main.py --yaml-path $script
+#    python -u main_test.py --yaml-path $script
+
+done
