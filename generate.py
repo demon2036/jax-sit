526e139982fb0ec5463939a9643f557b71016573
john <1165137798@qq.com>
2025-03-19 22:59:51 +0800

    commit

--- generate.py
+++ generate.py
@@ -244,7 +244,7 @@ def test_convert(args):
         pattern=shard_filename,
         maxcount=data_per_shard,
         maxsize=3e10,
-        start_shard=jax.process_index()+(5791//jax.process_count())*jax.process_count(),
+        start_shard=jax.process_index()+(6431//jax.process_count())*jax.process_count(),
         verbose=jax.process_index() == 0,
         progress_count=jax.process_count()
         # maxsize=shard_size,
